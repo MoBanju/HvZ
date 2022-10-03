9eb37865b98230236acc620b0986f55849af1866
synnems <synne@saevik.com>
2022-10-03 15:48:26 +0200

    style: landing page background

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,18 @@
+import { Container, Row, Col } from "react-bootstrap";
 import GamesTable from "../components/landingPage/GamesTable";
 import LoginForm from "../components/landingPage/LoginForm";
+import "./LandingPage.css";
 
 function LandingPage() {
-    return (<>
-    <h1>LANDING PAGE</h1> 
-    <LoginForm />
-    <GamesTable />
-    </>)
+    return (
+    <Container className="justify-content-center align-items-center test" fluid>
+        <Row> 
+            <Col> Hie </Col>
+            <Col> Hade</Col>
+        </Row>
+        <LoginForm />
+        <GamesTable />
+    </Container>)
 }
 
 
