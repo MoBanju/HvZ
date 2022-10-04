8c24c2920d79af0bd541118db777c4a6b1537eee
Synne Sævik <synne@saevik.com>
2022-10-04 10:42:46 +0000

    Ss landing page styling

--- src/components/landingPage/LoginForm.tsx
+++ src/components/landingPage/LoginForm.tsx
@@ -1,7 +1,16 @@
+import "../../pages/LandingPage.css";
+
 function LoginForm() {
-    return (<>
-    <p>Login form</p>
-    </>);
+    return (
+    <>
+    <div>
+        <button className="btn btn-dark btn-lg login-btn m-5">Login</button>
+    </div>
+    <div>
+        <button className="btn btn-dark btn-lg login-btn m-5">Register</button>
+    </div>
+    </>
+    );
 }
 
 export default LoginForm;
