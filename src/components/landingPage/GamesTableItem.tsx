f69f32d25bd70d6870dba62445e5564551d7614f
Eivind Bertelsen <eivind.bertelsen@no.experis.com>
2022-10-05 08:43:54 +0000

    feat: admin buttons

--- src/components/landingPage/GamesTableItem.tsx
+++ src/components/landingPage/GamesTableItem.tsx
@@ -1,17 +1,29 @@
 import { IGame } from "../../models/IGame";
 import "../../pages/LandingPage.css";
-import {CgTrash} from "react-icons/cg" 
+import { CgTrash } from "react-icons/cg"
+import keycloak from "../../keycloak";
+import {removeGame} from "../../store/slices/gamesSlice"
+import { useAppDispatch } from "../../store/hooks";
 
 
 
-function GamesTableItem({game}: {game: IGame}) {
-    return (<>
-            <tr className="ms-6">
-                    <td> {game.title} </td>
-                    <td>22</td>
-                    <td>{game.state}</td>
-                    <td><button className="btn-delete"><CgTrash/></button> </td>
-            </tr>
-    </>)
+function GamesTableItem({ game }: { game: IGame }) {
+        const isAdmin = keycloak.realmAccess?.roles.includes("ADMIN")
+        const dispatch = useAppDispatch()
+
+        const deleteGame = () => {
+                dispatch(removeGame(game.id))
+        }
+
+        return (<>
+                <tr className="ms-6">
+                        <td> {game.title} </td>
+                        <td>22</td>
+                        <td>{game.state}</td>
+                        <td>{isAdmin && <button onClick={deleteGame} className="btn-delete"><CgTrash /></button>} </td>
+                </tr>
+        </>)
+
+
 }
 export default GamesTableItem;
