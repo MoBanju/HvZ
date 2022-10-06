335ef7eb7a09f9486bb5dcea7c1060e9c39c9bff
synnems <synne@saevik.com>
2022-10-06 09:28:54 +0200

    fix: game details bitecode role view

--- src/components/gameDetailsPage/BiteCode.tsx
+++ src/components/gameDetailsPage/BiteCode.tsx
@@ -1,19 +1,30 @@
 import { IPlayer } from "../../models/IPlayer"
 import {IoIosArrowDroprightCircle} from "react-icons/io";
+import { IGameState } from "../../models/IGameState";
 
-function BiteCode({player}:{player: IPlayer}) {
-  return (
-    <>
-      <div>
-        <label className="me-2 fs-2" htmlFor="bitecode-input">Victims bitecode: </label>
-          <input className="rounded mt-3 mb-3 p-2 w-25" type="text" placeholder="Enter bitecode.." name="bitecode-input"/>
-          <button className="btn-delete"><IoIosArrowDroprightCircle size={40}/></button>
-      </div>
-
-      <div>
-        <p className="fs-2">Your bitecode: { <span className="bg-black bg rounded p-3 m-2 text-white text-center w-25">{player.biteCode}</span>}</p>
-      </div>
-    </>
+function BiteCode({player, gamestate}:{player: IPlayer, gamestate: keyof IGameState}) {
+  if (gamestate === 'inprogress'){
+    return (
+      <>
+      {!player.isHuman && <div>
+          <label className="me-2 fs-2" htmlFor="bitecode-input">Victims bitecode: </label>
+            <input className="rounded mt-3 mb-3 p-2 w-25" type="text" placeholder="Enter bitecode.." name="bitecode-input"/>
+            <button className="btn-delete"><IoIosArrowDroprightCircle size={40}/></button>
+        </div>}
+  
+        <div>
+          { player.isHuman && <p className="fs-2">Your bitecode: { <span className="bg-black bg rounded p-3 m-2 text-white text-center w-25">{player.biteCode}</span>}</p>}
+        </div>
+      </>
+    )
+  }
+  if(gamestate === 'register'){
+    return(
+      <div>Waiting for game..</div>
+    )
+  }
+  return(
+    <div></div>
   )
 }
 
