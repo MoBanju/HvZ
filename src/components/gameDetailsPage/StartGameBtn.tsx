e984ed07cf4e09b99309cb705ce17f0a2d801ee0
Eivind Bertelsen <eivind.bertelsen@no.experis.com>
2022-10-13 14:28:41 +0200

    chore: startgamebtn

--- src/components/gameDetailsPage/StartGameBtn.tsx
+++ src/components/gameDetailsPage/StartGameBtn.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import keycloak from '../../keycloak';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 
 
 function StartGameBtn() {
 
-    //Endre game-state fra register til in-progress
+  //Endre game-state fra register til in-progress
 
-    //PUT Games with game-id
+  //PUT Games with game-id
+  const isAdmin = keycloak.realmAccess?.roles.includes("ADMIN")
+  const { game, currentPlayer, players } = useAppSelector(state => state.game)
+  const dispatch = useAppDispatch();
 
+  if (game?.state === "Registration" && isAdmin) {
 
-  return (
-    <>
-    <button className="btn btn-dark mt-3 mb-3">Start Game</button>
-    </>)
+    return (
+      <>
+        <button className="btn btn-dark mt-3 mb-3">Start Game</button>
+      </>)
+  }
+  else{
+    return (
+      <><div>startgameBtn var her</div></>
+    )
+  }
 }
 
 export default StartGameBtn
