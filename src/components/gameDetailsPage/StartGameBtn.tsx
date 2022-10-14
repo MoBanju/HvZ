b61c07cca6c0b457b8eeedbf9e030fa16cf90f2a
Eivind Bertelsen <eivind.bertelsen@no.experis.com>
2022-10-14 13:00:56 +0000

    feat: startgamebtn

--- src/components/gameDetailsPage/StartGameBtn.tsx
+++ src/components/gameDetailsPage/StartGameBtn.tsx
@@ -1,17 +1,53 @@
 import React from 'react'
+import { Spinner } from 'react-bootstrap';
+import keycloak from '../../keycloak';
+import { IGame } from '../../models/IGame';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { RequestsEnum } from '../../store/middleware/requestMiddleware';
+import { namedRequestInProgAndError } from '../../store/slices/requestSlice';
+import { PutGameByIdAction } from '../api/putGameById';
 
 
 function StartGameBtn() {
 
-    //Endre game-state fra register til in-progress
+  const { game } = useAppSelector(state => state.game)
+  const [loading, error] = namedRequestInProgAndError(useAppSelector(state => state.requests), RequestsEnum.PutGameById)
+  const isAdmin = keycloak.realmAccess?.roles.includes("ADMIN")
+  const dispatch = useAppDispatch();
 
-    //PUT Games with game-id
+  const startGame: any = () => {
+  if(game){
+    dispatch(PutGameByIdAction(game))
+  }
+
+//Litt prøving og feiling æææ
+/*     if (game) {
+      let newGame:IGame = {
+        id: game.id,
+        name: game.name,
+        description: game.description,
+        state: "Progress"
+      }
+      dispatch(PutGameByIdAction(newGame))
+    }
+    console.log(game?.name, game?.state)
+   */
+  }
 
 
-  return (
-    <>
-    <button className="btn btn-dark mt-3 mb-3">Start Game</button>
-    </>)
+  if (game?.state === "Registration" && isAdmin) {
+    return (
+      <>
+        {!loading ? (<button className="btn btn-dark mt-3 mb-3" onClick={startGame}>Start Game</button>)
+          : <Spinner animation="border" size={"sm"} />}
+      </>)
+  }
+
+  else {
+    return (
+      <><div>Game started!!</div></>
+    )
+  }
 }
 
 export default StartGameBtn
