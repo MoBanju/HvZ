cbc864877256f0c03d7a0f362237da28797646da
vebsun95 <veb.sundal@gmail.com>
2022-10-14 09:19:24 +0200

    feat: added leaflet depend

--- src/components/landingPage/CreateGameModal.tsx
+++ src/components/landingPage/CreateGameModal.tsx
@@ -1,70 +1,89 @@
 import React, { Dispatch, MutableRefObject, useRef, useState } from "react";
-import { Button, Form, FormControl, InputGroup, Modal, Spinner} from "react-bootstrap";
+import { Button, Form, FormControl, InputGroup, Modal, Spinner } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { RequestsEnum } from "../../store/middleware/requestMiddleware";
 import { namedRequestInProgAndError } from "../../store/slices/requestSlice";
 import { PostGameAction, PostGameRequest } from "../api/postGames";
 
+import {MapContainer, SVGOverlay, TileLayer} from 'react-leaflet'
+
+const bounds = [
+  [51.49, -0.08],
+  [51.5, -0.06],
+]
 
 interface IProps {
     show: boolean,
     setShow: Dispatch<React.SetStateAction<boolean>>,
 }
 
-function CreateGameModal({show, setShow}: IProps) {
-    const hide = () => {setShow(false);}
+function CreateGameModal({ show, setShow }: IProps) {
+    const hide = () => { setShow(false); }
     const [loading, error] = namedRequestInProgAndError(useAppSelector(state => state.requests), RequestsEnum.PostGame)
 
     const nameInputRef = useRef() as MutableRefObject<HTMLInputElement>;
     const descriptionInputRef = useRef() as MutableRefObject<HTMLTextAreaElement>;
-    
+
     const dispatch = useAppDispatch();
 
-    const submitGame = () => {  
+    const submitGame = () => {
         const game: PostGameRequest = {
             name: nameInputRef.current.value,
             description: descriptionInputRef.current.value,
         }
-        const postGameAction = PostGameAction(game, () => {hide()})
+        const postGameAction = PostGameAction(game, () => { hide() })
         dispatch(postGameAction)
-      };
+    };
 
     return (
-    <Modal show={show} onEscapeKeyDown={hide} onHide={hide} id="myModal">
-        <Modal.Header closeButton>
-            <Modal.Title>Create a new game</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <InputGroup >
-                <InputGroup.Text id="title" >Title:</InputGroup.Text>
-                <FormControl
-                placeholder="HvZ summer camp 2025"
-                aria-label="Title"
-                aria-describedby="title"
-                ref={nameInputRef}
-                />
-            </InputGroup>
-            <InputGroup className="mt-4">
-                <InputGroup.Text >Description</InputGroup.Text>
-                <Form.Control 
-                as="textarea" 
-                aria-label="Description" 
-                placeholder="Come join your friends and family as we play a game of human vs zombies during summer camp 2025"
-                ref={descriptionInputRef}
-            />
-            </InputGroup>
-        </Modal.Body>
-        <Modal.Footer>
-            {error &&
-                <p>{error.message}</p>
-            }
-            <Button variant="dark" onClick={submitGame}>
-                {loading ?
-                <Spinner animation="border" as="span"/>
-                : <span>Submit</span>}
-            </Button>
-        </Modal.Footer>
-    </Modal>);
+        <Modal show={show} onEscapeKeyDown={hide} onHide={hide} id="myModal">
+            <Modal.Header closeButton>
+                <Modal.Title>Create a new game</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <InputGroup >
+                    <InputGroup.Text id="title" >Title:</InputGroup.Text>
+                    <FormControl
+                        placeholder="HvZ summer camp 2025"
+                        aria-label="Title"
+                        aria-describedby="title"
+                        ref={nameInputRef}
+                    />
+                </InputGroup>
+                <InputGroup className="mt-4">
+                    <InputGroup.Text >Description</InputGroup.Text>
+                    <Form.Control
+                        as="textarea"
+                        aria-label="Description"
+                        placeholder="Come join your friends and family as we play a game of human vs zombies during summer camp 2025"
+                        ref={descriptionInputRef}
+                    />
+                </InputGroup>
+                <MapContainer zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <SVGOverlay attributes={{ stroke: 'red' }} bounds={bounds}>
+                        <rect x="0" y="0" width="100%" height="100%" fill="blue" />
+                        <circle r="5" cx="10" cy="10" fill="red" />
+                        <text x="50%" y="50%" stroke="white">
+                            text
+                        </text>
+                    </SVGOverlay>
+                </MapContainer>,
+            </Modal.Body>
+            <Modal.Footer>
+                {error &&
+                    <p>{error.message}</p>
+                }
+                <Button variant="dark" onClick={submitGame}>
+                    {loading ?
+                        <Spinner animation="border" as="span" />
+                        : <span>Submit</span>}
+                </Button>
+            </Modal.Footer>
+        </Modal>);
 }
 
 export default CreateGameModal;
