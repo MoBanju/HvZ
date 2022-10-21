b531d69f54db1a1022971c45061310cde848f4e6
Øyvind Sande Reitan <oyvind.reitan@no.experis.com>
2022-10-21 17:57:58 +0200

    fix: errormessage now correctly displays in postgame validation

--- src/components/api/postGames.ts
+++ src/components/api/postGames.ts
@@ -28,8 +28,28 @@ async function postGame({postGameRequest}: IParams): Promise<IGame>{
         headers,
         body: JSON.stringify(postGameRequest),
     });
-    if(!response.ok)
-        throw new Error(await response.text() || response.statusText);
+    if(!response.ok){
+        var jsonError = await response.json();
+        console.log(jsonError);
+        var stringRequest = "";
+
+        if(jsonError){
+            if(jsonError.errors){
+                Object.entries(jsonError.errors).forEach((entry) => {
+                    const [key, value] = entry;
+                    console.log(value);
+                    if(value instanceof Array){
+                        if(value.length > 0){
+                            stringRequest += `${value[0]}\n`
+                        }
+                    }
+                });
+            }else{
+                stringRequest=jsonError;
+            }
+        }
+        throw new Error(stringRequest || response.statusText);
+    }
     let createdGame = await response.json() as IGame;
     return createdGame;
 };
