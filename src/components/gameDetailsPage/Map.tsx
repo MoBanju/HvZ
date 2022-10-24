714d2a9a0c7fe6c9cbbb153659d6a22a62131f28
MBanja <MBanja@N-NO-01-04-8521>
2022-10-24 15:25:03 +0200

    feat: Try to get the right zom with recursive function

--- src/components/gameDetailsPage/Map.tsx
+++ src/components/gameDetailsPage/Map.tsx
@@ -11,16 +11,49 @@ import Mission from './Mission';
 
 
 function Map({gameid} : {gameid: number}) {
+    /*
+    useEffect(() => {
+        if (!map) return;
+        console.log(map.getBounds());
+      }, [map]);
+    */
+    //const m = useMap();
+    //console.log(m.getBounds())
+    console.log(map)
     const { game } = useAppSelector(state => state.game)
     const center = [(game!.ne_lat + game!.sw_lat) / 2, (game!.ne_lng + game!.sw_lng) / 2] as LatLngTuple
     const areaG = (game!.ne_lng - game!.sw_lng) * (game!.ne_lat - game!.sw_lat)
+    const areaGReverse = (game!.ne_lng + game!.sw_lng) * (game!.ne_lat + game!.sw_lat)
     const times = 10
-    console.log(areaG)
+    const zoomN = 0
+    
+    console.log("Area: " + areaG)
+    console.log("Area Reverse: " + areaGReverse)
+    console.log("Center: " + center)
     console.log("x: " + game!.ne_lat + " + " + game!.sw_lat)
     console.log("y: " + game!.ne_lng + " + " + game!.sw_lng)
-    console.log(areaG + times)
+    console.log(areaGReverse - (areaGReverse* 0.99))
+
+    // Beginning Test
+    function InsideCart(z:number): number
+    {
+        if(z >= 13)
+        {
+            return InsideCart(z -1)
+        }
+        else
+        {
+            return z
+        }
+        
+    }
+
+
+    //const zoomN = (areaGReverse == 1343 || areaGReverse == 1324 || areaGReverse == 1343)
+    
+    // End Test
     return (
-    <MapContainer  center={center} zoom={ 13 } scrollWheelZoom={true} maxZoom={18} minZoom={9} style={{
+    <MapContainer  center={center} zoom={ InsideCart(20) } scrollWheelZoom={true} /* maxZoom={18} minZoom={9} */ style={{
         height: "30vh",
         width: "35vh",
       }} maxBounds={[[game!.sw_lat - 0.1, game!.sw_lng - 0.1], [game!.ne_lat + 0.1, game!.ne_lng + 0.1]]}>
